38de2e9b1c454504afdd82f4d7d96ac80c461be5
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-29 16:05:15 +0900

    letter_openerのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   resources :tasks
   resources :users
   resources :categories, only: %i(index create destroy edit update)
