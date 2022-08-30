08cc08507a367427f354fe26d8e2edd1b4b7d5d6
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-30 12:47:20 +0900

    rubocop

--- app/models/security.rb
+++ app/models/security.rb
@@ -8,10 +8,10 @@ class Security < ApplicationRecord
   def self.digest(string)
     # ハッシュ生成のコスト(＝安全性) テスト環境:低, 本番環境:高
     cost = if ActiveModel::SecurePassword.min_cost
-              BCrypt::Engine::MIN_COST
-            else
-              BCrypt::Engine.cost
-            end
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
     BCrypt::Password.create(string, cost:)
   end
 end
