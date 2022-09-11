18f67a89318a2301636e85bd97773f79d3562512
smptmhr <tomohiro.shimpo@gmail.com>
2022-09-11 19:29:50 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,28 +19,30 @@ class ApplicationController < ActionController::Base
     redirect_to request.referer
   end
 
+  # rubocop:disable Naming/VariableNumber
   # 例外処理
-  unless Rails.env.production?
+  unless Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
   end
- 
+
   def routing_error
     raise ActionController::RoutingError, params[:path]
   end
- 
+
   private
- 
+
   def render_404
     render 'error/404', status: :not_found
   end
 
-  def render_404
-    render 'error/404', status: :unprocessable_entity
+  def render_422
+    render 'error/422', status: :unprocessable_entity
   end
- 
+
   def render_500
     render 'error/500', status: :internal_server_error
   end
+  # rubocop:enable Naming/VariableNumber
 end
