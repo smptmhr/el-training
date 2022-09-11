6b9ee1f0f3a74a3f1e5d1d3f618dbf8930643e49
smptmhr <tomohiro.shimpo@gmail.com>
2022-09-11 18:40:03 +0900

    タスク一覧ページでラベルのN+1に対応

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
 
   def index
     # 現在ログイン中のユーザのタスクを取得
-    tasks = current_user.tasks.preload(:category).all # N+1対策でpreloadを使用
+    tasks = current_user.tasks.preload(:category, :labels).all # N+1対策でpreloadを使用
 
     # タスクの検索
     searched_tasks = tasks.search_task(params[:search], params[:search_option])
