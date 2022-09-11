ef30d36e90376a561af0a03fb5aec4f3c2b53be4
smptmhr <tomohiro.shimpo@gmail.com>
2022-09-11 18:59:12 +0900

    エラーページでI18nを使うためにhtml.erbにファイルを移行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,25 @@ class ApplicationController < ActionController::Base
     flash[:danger] = I18n.t 'admin_user_must_exist'
     redirect_to request.referer
   end
+
+  # 例外処理
+  unless Rails.env.production?
+    rescue_from Exception,                      with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
+ 
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+ 
+  private
+ 
+  def render_404
+    render 'error/404', status: :not_found
+  end
+ 
+  def render_500
+    render 'error/500', status: :internal_server_error
+  end
 end
